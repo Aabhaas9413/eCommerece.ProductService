0d709ddcdee575e8ee1590aa89fe02c3733ce95e
AABHAAS MALHOTRA <aabhaas9413@gmail.com>
2025-11-10 22:47:55 +0000

    Adding fluent migrations for add and update product.

---
+++ BusinessLogicLayer/Validators/ProductAddRequestValidator.cs
@@ -0,0 +1,26 @@
+﻿using BusinessLogicLayer.DTO;
+using FluentValidation;
+
+namespace BusinessLogicLayer.Validators;
+
+public class ProductAddRequestValidator : AbstractValidator<ProductAddRequest>
+{
+    public ProductAddRequestValidator()
+    {
+        RuleFor(x => x.ProductName)
+            .NotEmpty().WithMessage("Product name is required.")
+            .MinimumLength(2).WithMessage("Product name must be at least 2 characters long.")
+            .MaximumLength(200).WithMessage("Product name must not exceed 200 characters.");
+
+        RuleFor(x => x.Categorys)
+            .IsInEnum().WithMessage("Category must be a valid value.");
+
+        RuleFor(x => x.UnitPrice)
+            .GreaterThanOrEqualTo(0).WithMessage("Unit price must be 0 or greater.");
+
+        RuleFor(x => x.QuantityInStock)
+            .GreaterThanOrEqualTo(0)
+            .When(x => x.QuantityInStock.HasValue)
+            .WithMessage("Quantity in stock cannot be negative.");
+    }
+}
---
+++ BusinessLogicLayer/Validators/ProductUpdateRequestValidator.cs
@@ -0,0 +1,29 @@
+﻿using BusinessLogicLayer.DTO;
+using FluentValidation;
+
+namespace BusinessLogicLayer.Validators;
+
+public class ProductUpdateRequestValidator : AbstractValidator<ProductUpdateRequest>
+{
+    public ProductUpdateRequestValidator()
+    {
+        RuleFor(x => x.ProductID)
+            .NotEmpty().WithMessage("Product ID is required.");
+
+        RuleFor(x => x.ProductName)
+            .NotEmpty().WithMessage("Product name is required.")
+            .MinimumLength(2).WithMessage("Product name must be at least 2 characters long.")
+            .MaximumLength(200).WithMessage("Product name must not exceed 200 characters.");
+
+        RuleFor(x => x.Categorys)
+            .IsInEnum().WithMessage("Category must be a valid value.");
+
+        RuleFor(x => x.UnitPrice)
+            .GreaterThanOrEqualTo(0).WithMessage("Unit price must be 0 or greater.");
+
+        RuleFor(x => x.QuantityInStock)
+            .GreaterThanOrEqualTo(0)
+            .When(x => x.QuantityInStock.HasValue)
+            .WithMessage("Quantity in stock cannot be negative.");
+    }
+}
